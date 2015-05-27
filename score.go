894c0445c520965f438d671b269fd3d1f630dc64
Douglas Green <bluntsporks+git@gmail.com>
2015-05-27 08:44:09 -0400

    Moving LevDist func

--- score.go
+++ score.go
@@ -1,8 +1,10 @@
 package main
 
+import "github.com/BluntSporks/string-functions"
+
 // Score returns the score of a user response on a scale from 0 to 100.
 func Score(correct string, resp string) int {
-	dist := LevDist(correct, resp)
+	dist := strfunc.LevDist(correct, resp)
 	prop := 1.0 - float64(dist)/float64(len(correct))
 	return int(prop * 100.0)
 }
